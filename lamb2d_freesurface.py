5fa110fbfcbec5bcb5140f9a52226cb3f27a1fed
Ki-Tae Kim <qlsn55@gmail.com>
2020-09-26 22:00:59 -0700

    small modification

--- lamb2d_freesurface.py
+++ lamb2d_freesurface.py
@@ -1,3 +1,19 @@
+#  2D Lamb's problem
+#  Copyright © 2020 Ki-Tae Kim
+
+#  This library is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this library; if not, see <http://www.gnu.org/licenses/>.
+
 import math
 import numpy as np
 import scipy.optimize
